46e0122e4cdc387260925221ccb2a2151e2b7b8d
DanXaPd <92036667+DanXaPd@users.noreply.github.com>
2022-02-17 23:44:38 +0200

    ex_funktion

---
+++ main.js
@@ -0,0 +1,38 @@
+// function_1
+// function display_massege() {
+//     alert("Сообщение по рассматриваемой теме")
+// }
+// display_massege()
+
+// function_2
+// function favourite_film(title) {
+//     alert(`Твой любимый фильм это ${title}`)
+// }
+// let film = prompt("Какой ваш любимый фильм?")
+// favourite_film(film)
+
+// function_3
+// function make_shirt(size, txt) {
+//     alert(`Размер вашей футболки: ${size}, а принт на ней будет: ${txt}`)
+// }
+// let razmer = prompt("Введите размер футболки")
+// let print = prompt("Введите надпись на футбюолку")
+// make_shirt(razmer, print)
+
+// function_4
+// function make_shirt(size, txt) {
+//     alert(`Размер первой футболки L, а принт на ней будет "Я люблю JavaScript"`)
+//     alert(`Размер второй футболки: ${size}, а принт на ней будет: "${txt}"`)
+// }
+// alert("Первая футболка идет по умолчанию")
+// let razmer = prompt("Введите размер второй футболки")
+// let print = prompt("Введите надпись на вторую футбюолку")
+// make_shirt(razmer, print)
+
+// function_5
+// function describe_city(city, country) {
+//     alert(`Город ${city} находиться в стране под названием ${country}`)
+// }
+// let gorod = prompt("Введите город")
+// let strana = prompt("Введите страну в которой находиться этот город")
+// describe_city(gorod, strana)
